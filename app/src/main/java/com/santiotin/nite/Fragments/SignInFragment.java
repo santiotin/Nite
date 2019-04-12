3ff0bf0871b11322fe48f7bb7686ebd237c2688e
santiotin <santiotin@users.noreply.github.com>
2019-04-13 00:42:01 +0200

    Login de Google funciona cuando le da la gana

--- app/src/main/java/com/santiotin/nite/Fragments/SignInFragment.java
+++ app/src/main/java/com/santiotin/nite/Fragments/SignInFragment.java
@@ -58,7 +58,7 @@ public class SignInFragment extends Fragment {
 
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.web_client_id))
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
