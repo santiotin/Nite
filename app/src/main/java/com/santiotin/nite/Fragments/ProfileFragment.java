6d07c855ca3c0b1abd5819ade8cf1418637e67b8
santiotin <santiotin@users.noreply.github.com>
2019-04-14 17:22:30 +0200

    PopupMenu añadido para el profile

--- app/src/main/java/com/santiotin/nite/Fragments/ProfileFragment.java
+++ app/src/main/java/com/santiotin/nite/Fragments/ProfileFragment.java
@@ -5,11 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -46,39 +50,54 @@ public class ProfileFragment extends Fragment {
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
 
-        ImageButton btnSettings = view.findViewById(R.id.btnSettings);
+        final ImageButton btnSettings = view.findViewById(R.id.btnSettings);
         TextView name = view.findViewById(R.id.name);
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(getActivity(), btnSettings);
+                popupMenu.getMenuInflater().inflate(R.menu.settings_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.logout:
+                                mAuth.signOut();
+
+                                //sign out de la cuenta de google
+                                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                                        .requestIdToken(getString(R.string.default_web_client_id))
+                                        .requestEmail()
+                                        .build();
+
+                                mGoogleSignInClient = GoogleSignIn.getClient(getContext(), gso);
+                                mGoogleSignInClient.signOut();
+
+                                //finalizar activity e ir al login
+                                getActivity().finish();
+                                Intent intent = new Intent(getContext(), LoginActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                                return true;
+
+                            case R.id.editprofile:
+                                return true;
+
+                            default:
+                                return false;
+                        }
+                    }
+                });
+                popupMenu.show();
+            }
+        });
 
         if(user.getDisplayName() != null) {
             name.setText(user.getDisplayName());
         }
 
-
-
-        btnSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // sign out de la cuenta de firebase
-                mAuth.signOut();
-
-                //sign out de la cuenta de google
-                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                        .requestIdToken(getString(R.string.default_web_client_id))
-                        .requestEmail()
-                        .build();
-
-                mGoogleSignInClient = GoogleSignIn.getClient(getContext(), gso);
-                mGoogleSignInClient.signOut();
-
-                //finalizar activity e ir al login
-                getActivity().finish();
-                Intent intent = new Intent(v.getContext(), LoginActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }
-        });
-
         return  view;
-    }
 
+    }
 }
