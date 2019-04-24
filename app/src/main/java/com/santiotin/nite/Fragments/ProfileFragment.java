7f34b2e47bc5545fd5772f1a152d6f964c154991
santiotin <santiotin@users.noreply.github.com>
2019-04-24 13:29:04 +0200

    Sigo cambiando de perfil

--- app/src/main/java/com/santiotin/nite/Fragments/ProfileFragment.java
+++ app/src/main/java/com/santiotin/nite/Fragments/ProfileFragment.java
@@ -3,35 +3,25 @@ package com.santiotin.nite.Fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
-import android.view.ContextMenu;
 import android.view.LayoutInflater;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.santiotin.nite.EditProfileActivity;
 import com.santiotin.nite.LoginActivity;
-import com.santiotin.nite.MyDrinksActivity;
 import com.santiotin.nite.MyEventsActivity;
 import com.santiotin.nite.MyFriendsActivity;
-import com.santiotin.nite.MyTicketsActivity;
 import com.santiotin.nite.R;
 
 
@@ -60,25 +50,10 @@ public class ProfileFragment extends Fragment {
         final ImageButton btnSettings = view.findViewById(R.id.btnSettings);
         TextView name = view.findViewById(R.id.name);
 
-        CardView cvTickets = view.findViewById(R.id.profileCardTickets);
-        CardView cvDrinks = view.findViewById(R.id.profileCardDrinks);
         CardView cvFriends = view.findViewById(R.id.profileCardFriends);
         CardView cvEvents = view.findViewById(R.id.profileCardEvents);
 
-        cvTickets.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), MyTicketsActivity.class);
-                startActivity(intent);
-            }
-        });
-        cvDrinks.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), MyDrinksActivity.class);
-                startActivity(intent);
-            }
-        });
+
         cvFriends.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -136,7 +111,7 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-        if(user.getDisplayName() != null && !user.getDisplayName().equals("")) {
+        if(user != null && user.getDisplayName() != null && !user.getDisplayName().equals("")) {
             name.setText(user.getDisplayName());
         }
 
--- app/src/main/java/com/santiotin/nite/PruebasActivity.java
+++ app/src/main/java/com/santiotin/nite/PruebasActivity.java
@@ -1,12 +1,24 @@
 package com.santiotin.nite;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.santiotin.nite.Fragments.SignInFragment;
 import com.santiotin.nite.Fragments.SignUpFragment;
 
@@ -15,10 +27,94 @@ import java.util.List;
 
 public class PruebasActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pruebas);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        final ImageButton btnSettings = findViewById(R.id.btnSettings);
+        TextView name = findViewById(R.id.name);
+
+        RelativeLayout rlevents = findViewById(R.id.rlmyevents);
+        RelativeLayout rlfollowing = findViewById(R.id.rlfollowing);
+        RelativeLayout rlfollowers = findViewById(R.id.rlfollowers);
+
+        rlevents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MyEventsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        rlfollowers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MyFriendsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        rlfollowing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MyFriendsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(getApplicationContext(), btnSettings);
+                popupMenu.getMenuInflater().inflate(R.menu.settings_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.logout:
+                                mAuth.signOut();
+
+                                //sign out de la cuenta de google
+                                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                                        .requestIdToken(getString(R.string.default_web_client_id))
+                                        .requestEmail()
+                                        .build();
+
+                                mGoogleSignInClient = GoogleSignIn.getClient(getApplicationContext(), gso);
+                                mGoogleSignInClient.signOut();
+
+                                //finalizar activity e ir al login
+                                finish();
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                                return true;
+
+                            case R.id.editprofile:
+                                Intent i = new Intent(getApplicationContext(), EditProfileActivity.class);
+                                startActivity(i);
+                                return true;
+
+                            default:
+                                return false;
+                        }
+                    }
+                });
+                popupMenu.show();
+            }
+        });
+
+        if(user.getDisplayName() != null && !user.getDisplayName().equals("")) {
+            name.setText(user.getDisplayName());
+        }
+
 
 
     }
