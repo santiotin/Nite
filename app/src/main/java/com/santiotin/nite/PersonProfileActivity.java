9102137305d3f57e954808bd95580c4d0edfe60a
santiotin <santiotin@users.noreply.github.com>
2019-05-06 13:00:46 +0200

    Sincronizado los amigos con la base de datos

--- app/src/main/java/com/santiotin/nite/PersonProfileActivity.java
+++ app/src/main/java/com/santiotin/nite/PersonProfileActivity.java
@@ -1,7 +1,9 @@
 package com.santiotin.nite;
 
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,9 +12,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -20,6 +25,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.mikhaellopez.circularimageview.CircularImageView;
+import com.santiotin.nite.Models.User;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PersonProfileActivity extends AppCompatActivity {
 
@@ -30,6 +39,9 @@ public class PersonProfileActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser user;
     private FirebaseFirestore db;
+    private User friendUser;
+
+    private Button followBtn;
 
     Boolean meSigue;
     Boolean leSigo;
@@ -44,17 +56,23 @@ public class PersonProfileActivity extends AppCompatActivity {
         user = mAuth.getCurrentUser();
         db = FirebaseFirestore.getInstance();
 
+
         uidFriend = getIntent().getStringExtra("uid");
         nameFriend = getIntent().getStringExtra("name");
         uriFriend = Uri.parse(getIntent().getStringExtra("uri"));
 
-
-
-
+        followBtn = findViewById(R.id.btnFollow);
 
         iniToolbar();
         iniCampos();
         consultarRelacion();
+
+        followBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showConfirmationDialog();
+            }
+        });
     }
 
     public void iniToolbar(){
@@ -148,24 +166,107 @@ public class PersonProfileActivity extends AppCompatActivity {
     }
 
     private void actualizarBoton(){
-        Button button = findViewById(R.id.btnFollow);
         if(leSigo){
-            button.setBackground(getResources().getDrawable(R.drawable.rectangle_pink));
-            button.setTextColor(getResources().getColor(R.color.white));
+            followBtn.setBackground(getResources().getDrawable(R.drawable.rectangle_pink));
+            followBtn.setTextColor(getResources().getColor(R.color.white));
             if(meSigue){
-                button.setText(R.string.following);
+                followBtn.setText(R.string.following);
             }else{
-                button.setText(R.string.followin);
+                followBtn.setText(R.string.followin);
             }
         }
         else {
-            button.setBackground(getResources().getDrawable(R.drawable.rectangle_white_pink));
-            button.setTextColor(getResources().getColor(R.color.pink2));
+            followBtn.setBackground(getResources().getDrawable(R.drawable.rectangle_white_pink));
+            followBtn.setTextColor(getResources().getColor(R.color.pink2));
             if(meSigue){
-                button.setText(R.string.teSigue);
+                followBtn.setText(R.string.teSigue);
             }else{
-                button.setText(R.string.follow);
+                followBtn.setText(R.string.follow);
             }
         }
+    }
+
+    public void showConfirmationDialog(){
+        if (leSigo){
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(PersonProfileActivity.this)
+                    .setTitle("Dejar de seguir")
+                    .setMessage("¿Quieres dejar de seguir a este usuario?")
+
+                    // Specifying a listener allows you to take an action before dismissing the dialog.
+                    // The dialog is automatically dismissed when a dialog button is clicked.
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Continue with delete operation
+                            db.collection("friendship")
+                                    .document(user.getUid() + uidFriend)
+                                    .delete()
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Log.d("control", "DocumentSnapshot successfully deleted!");
+                                            leSigo = false;
+                                            actualizarBoton();
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w("control", "Error deleting document", e);
+                                        }
+                                    });
+
+
+                        }
+                    })
+                    // A null listener allows the button to dismiss the dialog and take no further action.
+                    .setNegativeButton(android.R.string.no, null)
+                    .setIcon(R.drawable.ic_calendar_black);
+            AlertDialog alert1 = builder.create();
+            alert1.show();
+        }else{
+            AlertDialog.Builder builder = new AlertDialog.Builder(PersonProfileActivity.this)
+                    .setTitle("Empezar a seguir")
+                    .setMessage("¿Quieres empezar a seguir a este usuario?")
+
+                    // Specifying a listener allows you to take an action before dismissing the dialog.
+                    // The dialog is automatically dismissed when a dialog button is clicked.
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Continue with delete operation
+                            Map<String, Object> assist = new HashMap<>();
+                            assist.put("followerName", user.getDisplayName());
+                            assist.put("followerUid", user.getUid());
+                            assist.put("followingName", nameFriend);
+                            assist.put("followingUid", uidFriend);
+
+                            db.collection("friendship").document(user.getUid() + uidFriend)
+                                    .set(assist)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Log.d("control", "DocumentSnapshot successfully written!");
+                                            leSigo = true;
+                                            actualizarBoton();
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w("control", "Error writing document", e);
+                                        }
+                                    });
+
+
+                        }
+                    })
+
+                    // A null listener allows the button to dismiss the dialog and take no further action.
+                    .setNegativeButton(android.R.string.no, null)
+                    .setIcon(R.drawable.ic_calendar_black);
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+
     }
 }
