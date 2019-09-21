7790321e416da33143fe3b0bdf007f21a6a78e7b
Santi <santiotin98@gmail.com>
2019-09-21 16:59:54 +0200

    Quito las funciones que ha de hacer la base de datos

--- app/src/main/java/com/santiotin/nite/Activities/EventDescriptionActivity.java
+++ app/src/main/java/com/santiotin/nite/Activities/EventDescriptionActivity.java
@@ -466,8 +466,33 @@ public class EventDescriptionActivity extends AppCompatActivity implements OnMap
         final Map<String, Object> userAssist = new HashMap<>();
         userAssist.put("userName", user.getDisplayName());
 
+        db.collection("events")
+                .document(event.getId())
+                .collection("assistingUsers")
+                .document(user.getUid())
+                .set(userAssist)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("control", "DocumentSnapshot successfully written!");
+                        assistPressed = true;
+                        changeAssistButtonState();
+                        //sendHistoryNotification();
+                        //sendEventNotification();
+                        //transactionIncrementAssitants();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("control", "Incoherencia!!!!!", e);
+                        assistPressed = false;
+                        changeAssistButtonState();
+                    }
+                });
 
-        db.collection("users")
+
+        /*db.collection("users")
                 .document(user.getUid())
                 .collection("assistingEvents")
                 .document(event.getId())
@@ -508,7 +533,7 @@ public class EventDescriptionActivity extends AppCompatActivity implements OnMap
                         assistPressed = false;
                         changeAssistButtonState();
                     }
-                });
+                });*/
 
 
     }
